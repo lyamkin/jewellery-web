d7f23a05d1fcac9d3c1cebcdb86322ce36478ce5
Andrey Lyamkin <lyamkin.andr@gmail.com>
2020-08-10 13:34:01 -0400

    Build responsive design

--- js/script.js
+++ js/script.js
@@ -3,10 +3,26 @@ $('#year').text(new Date().getFullYear());
 
  // Light box
 
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
 
 //Scrollspy
 $('body').scrollspy({target: '#main-nav'});
+
+// Smooth Scrolling
+$("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        window.location.hash = hash;
+      });
+    }
+  });
